2ace5e3bb7141db9345b91f016f31c80d2ec2419
truong-cot <engtruong1609@gmail.com>
2022-08-04 11:10:13 +0700

    cấu hình scss, bootstrap 4

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ app.engine('hbs', handlebars.engine({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
+// Config static file (http://localhost:8000 =>> là thư mục public)
+app.use(express.static(path.join(__dirname, 'public')));
+
 // HTTP logger
 app.use(morgan('combined'));
 
