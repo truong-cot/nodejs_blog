2e782b62ac4788ee4b957996d8dc76dba2338383
truong-cot <engtruong1609@gmail.com>
2022-08-04 10:16:17 +0700

    cấu hình handlebars, partials

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+
+const app = express();
+const port = 8000;
+
+// Template engine => defaultLayout(views/layout/main), config extname => .hbs
+app.engine('hbs', handlebars.engine({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Create page
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.get('/news', (req, res) => {
+	res.render('news');
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port http://localhost:${port}`);
+});
